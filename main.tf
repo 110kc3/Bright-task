a80bf4a28848a248f4868a857b307042d5a3990d
KC <110kc3@gmail.com>
2023-03-30 11:46:37 +0200

    clean-up

--- main.tf
+++ main.tf
@@ -1,61 +1,30 @@
-# Terraform configuration for creating a VPC with a public subnet
-
-# Create a VPC
-resource "aws_vpc" "app_vpc" {
-  cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = "app-vpc"
-  }
-}
-# Create an Internet Gateway and associate it with the VPC
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  tags = {
-    Name = "vpc_igw"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
   }
 }
 
-# Create a public subnet in the VPC
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone = var.availability-zone
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-#creating another subnet in diferent zone
-resource "aws_subnet" "public_subnet2" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = var.public_subnet_cidr2
-  map_public_ip_on_launch = true
-  availability_zone = var.availability-zone2
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-# Create a route table for the public subnet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
+provider "aws" {
+  region     = var.region
+  # credentials_file = var.creds
+  #  = "C:/repos/Bright-task/my-key-pair" 
+  # secret_key = var.secret_key
 }
 
-# Associate the public subnet with the route table
-resource "aws_route_table_association" "public_rt_asso" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+module "web-server" {
+  source ="./web-server"
+}
+
+module "network"{
+  source = "./network"
+}
+
+module "database"{
+  source = "./database"
+}
+
+module "load-balancer"{
+  source = "./load-balancer"
 }
